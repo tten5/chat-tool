a1c90fe905b78e349df2b93237ff57f93c7742c5
tten5 <pham.thuy.tien.vq@tut.jp>
2022-04-19 18:08:30 +0900

    Add EC2 server to docs

--- backend/docs/servers.js
+++ backend/docs/servers.js
@@ -3,8 +3,12 @@ require('dotenv').config()
 module.exports = {
     servers: [
       {
-        url: process.env.EC2_IP || "http://localhost:3000/", // url
+        url: "http://localhost:3000/", // url
         description: "Local server", // name
+      },
+      {
+        url: process.env.EC2_IP || "http://localhost:3000", // url
+        description: "EC2 server", // name
       },
     ],
   };
